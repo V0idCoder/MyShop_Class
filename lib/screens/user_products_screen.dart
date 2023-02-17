91cf461865faa7446ecf49821366d9cd9eb60601
V0idCoder <114491282+V0idCoder@users.noreply.github.com>
2023-02-17 19:21:47 +0100

    Update user_products_screen.dart

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products_provider.dart';
-import 'add_edit_product_screen.dart';
+//import 'add_edit_product_screen.dart';
 import '../widgets/app_drawer.dart';
 import '../widgets/user_product_item.dart';
 
